bcbd4040db35be01dd02b588b0dff293b779498d
matthew <matthew.kolesnik@gmail.com>
2026-02-11 01:02:55 -0500

    my first commit for my cookie clicker dupe

---
+++ js/game.js
@@ -0,0 +1,40 @@
+const btn = document.getElementById("button");
+const scoreDis = document.getElementById("scoreDisplay");
+
+const gameState = {
+    souls: 0,
+    soulsPerClick: 1,
+
+    autoClickSpeed: 0,
+    autoClickTimerID: null,
+
+    upgrades:{
+        //Increases click value
+        scythe:{
+            name: "Scythe",
+            cost: 10,
+            multiplier: 2,
+            count: 0
+        },
+
+        //Auto CLickers
+        Charon: {
+         name: "Summon Charon",
+         cost: 100,
+         count: 0,
+         level: 0
+        }
+    },
+
+    achievments: [
+        { id: 'first100', threshold: 100, name: "You yearn for the souls", unlocked: false },
+        { id: 'first1000', threshold: 1000, name: "your still here? Thanks!", unlocked: false }
+    ]
+}
+
+btn.addEventListener("click", function() {
+
+    gameState.souls = gameState.souls + 1; 
+    scoreDis.innerHTML = gameState.souls;
+
+});
